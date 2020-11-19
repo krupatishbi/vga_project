759f800a14bcc271bc6a838d1fffbef07a9f5cf1
krupatishbi <72935073+krupatishbi@users.noreply.github.com>
2020-11-19 14:23:43 -0500

    Add files via upload
    triangle generator, peculiar problem where when I simulate, the state machine never seems to go to state done?

--- vga_project.srcs/sources_1/new/top.v
+++ vga_project.srcs/sources_1/new/top.v
@@ -24,9 +24,17 @@ module top(
         reset <= pre_reset;
     end
     
-    horizontal_counter vga_horiz(clk, reset, enable_V_counter, H_count_value);
-    vertical_counter vga_vert(clk, reset, enable_V_counter, V_count_value);
+    //values to use for simulation
+//    `define ACTIVE_Hend (63)
+//    `define FRONT_PORCH_Hend (65)
+//    `define SYNC_PULSE_Hend (75)
+//    `define BACKPORCH_Hend (79)
     
+//    `define ACTIVE_Vend (40)
+//    `define FRONT_PORCH_Vend (43)
+//    `define SYNC_PULSE_Vend (46)
+//    `define BACKPORCH_Vend (52)
+
     `define ACTIVE_Hend (639)
     `define FRONT_PORCH_Hend (655)
     `define SYNC_PULSE_Hend (751)
@@ -37,116 +45,126 @@ module top(
     `define SYNC_PULSE_Vend (491)
     `define BACKPORCH_Vend (520)
     
+    horizontal_counter #(.BACKPORCH_Hend(`BACKPORCH_Hend)) vga_horiz(clk, reset, enable_V_counter, H_count_value);
+    vertical_counter #(.BACKPORCH_Vend(`BACKPORCH_Vend)) vga_vert(clk, reset, enable_V_counter, V_count_value);
+  
+    
     //outputs
     assign VGA_HS = ~(H_count_value > `FRONT_PORCH_Hend && H_count_value <= `SYNC_PULSE_Hend) ? 1'b1:1'b0;
     assign VGA_VS = ~(V_count_value > `FRONT_PORCH_Vend && V_count_value <= `SYNC_PULSE_Vend) ? 1'b1:1'b0;
-   
-    reg [8:0] top; //y coord
-    reg [9:0] left; //x coord
-    reg [9:0] length;
-    reg [8:0] height;
-    reg [6:0] v_x; //x
-    reg [6:0] v_y; //y
+    
+    
+    
+    reg [3:0] present_state;
+    reg [3:0] next_state;
+    parameter idle = 5'b00001;
+    parameter edge_a = 5'b00010;
+    parameter edge_b = 5'b00100;
+    parameter edge_c = 5'b01000;
+    parameter done = 5'b10000;
+    reg [18:0] write_addr;
+    reg [18:0] read_addr;
+    reg [7:0] d_in;
+    wire [7:0] d_out;
+    reg write_enable;
+    reg [9:0] x0, x1, x2;
+    reg [8:0] y0, y1, y2;
+    reg [9:0] half_length;
+    reg [9:0] mar_x;
+    reg [8:0] mar_y;
+    
+    //define every color so you can mix intensities
     
     always @ (posedge clk)
     begin
         if(reset)
         begin
-            top <= 0;
-            left <= 0;
-            length <= 50;
-            height <= 50;
-            v_x <= 5;
-            v_y <= 4;
-        end
-        else if (H_count_value == 0 && V_count_value == 0)
-        begin
-            top <= top + v_y;
-            left <= left + v_x;
+            x0 <= 10'd20;
+            y0 <= 9'd460; //111001100
+            half_length <= 10'd100;
+            x1 <= x0 + (2*half_length);
+            y1 <= y0;
+            x2 <= x0 + (half_length);
+            y2 <= y0 - (half_length);
+            d_in <= 8'b11111111;
+            
         end
     end
     
-    always @ (*)
+    always @ (posedge clk)
     begin
-        //if-else statements assigning colors
-        //order of colors: black, white, red, yellow, green, cyan, blue, magenta
-//        if(H_count_value <=79)
-//        begin
-//            VGA_R = 4'h0;
-//            VGA_G = 4'h0;
-//            VGA_B = 4'h0;
-//        end        
-//        else if(H_count_value <=159)
-//        begin
-//            VGA_R = 4'hF;
-//            VGA_G = 4'hF;
-//            VGA_B = 4'hF;
-//        end
-//        else if(H_count_value <=239)
-//        begin
-//            VGA_R = 4'hF;
-//            VGA_G = 4'h0;
-//            VGA_B = 4'h0;
-//        end
-//        else if(H_count_value <=319)
-//        begin
-//            VGA_R = 4'hF;
-//            VGA_G = 4'hF;
-//            VGA_B = 4'h0;
-//        end
-//        else if(H_count_value <=399)
-//        begin
-//            VGA_R = 4'h0;
-//            VGA_G = 4'hF;
-//            VGA_B = 4'h0;
-//        end
-//        else if(H_count_value <=479)
-//        begin
-//            VGA_R = 4'h0;
-//            VGA_G = 4'hF;
-//            VGA_B = 4'hF;
-//        end
-//        else if(H_count_value <= 559)
-//        begin
-//            VGA_R = 4'h0;
-//            VGA_G = 4'h0;
-//            VGA_B = 4'hF;
-//        end
-//        else if(H_count_value <=639)
-//        begin
-//            VGA_R = 4'hF;
-//            VGA_G = 4'h0;
-//            VGA_B = 4'hF;
-//        end
-//        else
-//        begin
-//            VGA_R = 4'h0;
-//            VGA_G = 4'h0;
-//            VGA_B = 4'h0;
-//        end
-//        VGA_R = (H_count_value <= `ACTIVE_Hend && V_count_value <= `ACTIVE_Vend) ? 4'hF:4'h0;
-//        VGA_G = (H_count_value <= `ACTIVE_Hend && V_count_value <= `ACTIVE_Vend) ? 4'hF:4'h0;
-//        VGA_B = (H_count_value <= `ACTIVE_Hend && V_count_value <= `ACTIVE_Vend) ? 4'hF:4'h0;
-    
-    //animating a square
-    //basically just a box
-    if(H_count_value <= `ACTIVE_Hend && V_count_value <= `ACTIVE_Vend)
-    begin
-        if(V_count_value >= top && V_count_value <= (top + height) && H_count_value >= left && H_count_value <= (left + length))
+        if(reset)
         begin
-            VGA_R = 4'hf;
-            VGA_G = 4'hf;
-            VGA_B = 4'hf;
+            present_state <= idle;
+            mar_x <= x0;
+            mar_y <= y0;
+            write_enable <= 0;
+            read_addr <= 0;
+            write_addr <= 0;
+            VGA_R = 0;
+            VGA_G = 0;
+            VGA_B = 0;
         end
         else
         begin
-            VGA_R = 4'h0;
-            VGA_G = 4'h0;
-            VGA_B = 4'h0;
+            case(present_state)
+                idle:
+                    if(!reset)
+                    begin
+                        present_state <= edge_a;
+                        write_enable <= 1;
+                        mar_x <= x0;
+                        mar_y <= y0;
+                    end
+                        
+                edge_a:
+                begin
+                    mar_x <= mar_x + 1;
+                    if(mar_x == (x1-1))
+                        present_state <= edge_b;
+                end
+                edge_b:
+                begin
+                    mar_x <= mar_x - 1;
+                    mar_y <= mar_y - 1;
+                    if(mar_x == (x2+1))
+                        present_state <= edge_c;
+                    
+                end
+                edge_c:
+                begin
+                    mar_x <= mar_x - 1;
+                    mar_y <= mar_y + 1;
+                    if(mar_x == (x0+1))
+                        present_state <= done;
+                end
+                done:
+                begin
+                    x0 <= x0 + 4; //moves right 4
+                    y0 <= y0 - 4; //moves up 4
+                    if ( half_length > 20)
+                    begin
+                        half_length <= half_length - 8;
+                        present_state <= idle;
+                    end
+                    else
+                        write_enable <= 0;
+                end
+                default:
+                    present_state <= idle;
+            endcase
+            
+            read_addr <= {H_count_value[9:0], V_count_value[8:0]}; //addra
+            write_addr <= {mar_x, mar_y}; //addrb
+            
+            VGA_R <= d_out[7:5];
+            VGA_G <= d_out[4:2];
+            VGA_B <= d_out[1:0];
+        
         end
     end
-end 
     
+    blk_mem_gen_0 ram(clk, write_enable, write_addr, d_in, clk, read_addr, d_out);
+    clk_wiz_0 CLKWIZ0(.clk_out1(clk), .locked(locked), .clk_in1(clk_in1));
     
-    clk_wiz_0 CLKWIZ0(.clk_out1(clk), .resetn(rst_), .locked(locked), .clk_in1(clk_in1));
 endmodule
