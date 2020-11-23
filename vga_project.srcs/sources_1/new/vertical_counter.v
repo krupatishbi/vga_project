ca39da18b13a49ca14aaed5dd8fc1caebeac2d58
krupatishbi <72935073+krupatishbi@users.noreply.github.com>
2020-11-22 20:41:25 -0500

    updated parameter BACKPORCH_Vend for simulation timings

--- vga_project.srcs/sources_1/new/vertical_counter.v
+++ vga_project.srcs/sources_1/new/vertical_counter.v
@@ -2,7 +2,8 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 
-module vertical_counter(
+module vertical_counter #(BACKPORCH_Vend = 524)
+    (
     input clk,
     input reset,
     input enable_V_counter,
@@ -19,7 +20,7 @@ module vertical_counter(
     begin
         if(enable_V_counter == 1'b1)
         begin
-            if(V_count_value <524)
+            if(V_count_value < BACKPORCH_Vend)
                 V_count_value <= V_count_value + 1; //incrementing V counter
             else
                 V_count_value <= 0; //reset horizontal counter
